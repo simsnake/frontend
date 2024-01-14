c98157d15be9f0149b71fb4c62783723e3d7c476
StemPunks9254 <ericshen567890@gmail.com>
2024-01-14 01:03:50 -0800

    bruh2

--- build/src/App.js
+++ build/src/App.js
@@ -12,6 +12,7 @@ function App() {
   return (
     <>
       {
+<<<<<<< HEAD
         <div className="App">
           <div className="topMenu"></div>
           <div className="mainArea">
@@ -22,11 +23,27 @@ function App() {
             </ul>
           </div>
         </div >
+=======
+        themeLoaded && <ThemeProvider theme={selectedTheme}>
+          <GlobalStyles />
+          <Container style={{ fontFamily: selectedTheme.font }}>
+            <div className="App">
+              <div className="topMenu"></div>
+              <div className="mainArea">
+                <ul>
+                  {blocks.map(block => (
+                    <Block id={block}></Block>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </Container>
+        </ThemeProvider>
+>>>>>>> parent of b2c233b (added block offsetting)
       }
     </>
   );
 }
-
 
 export function spawnBlock() {
   const name = "block" + blocks.length;
